22f42a2062d349e16fc61c2c2c7729dfadb3bcfb
MuhammadSakti <indratouwe@gmail.com>
2022-07-25 21:39:46 +0700

    fix nodev to node

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const routes = require('./routes');
 const init = async () => {
   const server = Hapi.server({
     port: 5004,
-    host: process.env.NODEV_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     /**
      * Buat menghandle same origin policy
      * nanti baca lagi itu apa
